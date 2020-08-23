5d9241968ab716ffa5c77ec74bbb6d7ef85f0650
baizhang1003 <782257169@qq.com>
2020-08-23 10:42:52 +0800

    添加接口

---
+++ src/main/java/com/tianyuan/controller/NurseryManageController.java
@@ -0,0 +1,10 @@
+package com.tianyuan.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NurseryManageController extends BaseManageController {
+
+	
+	
+}
